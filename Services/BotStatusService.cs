fc3ff4118bc3f99e17dc89cdcf93e5e48ec4e210
VERMEIT3 <thomas.vermeij2@hva.nl>
2022-01-10 19:59:30 +0100

    BotStatusService code convention updated

--- Services/BotStatusService.cs
+++ Services/BotStatusService.cs
@@ -1,8 +1,5 @@
-﻿using Discord;
-using Discord.Addons.Hosting;
-using Discord.Addons.Hosting.Util;
-using Discord.WebSocket;
-using Microsoft.Extensions.Logging;
+﻿namespace Saer.Services;
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,28 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Addons.Hosting.Util;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
 
-namespace Saer.Services
+public class BotStatusService : DiscordClientService
 {
-    public class BotStatusService : DiscordClientService
+    public BotStatusService(DiscordSocketClient client, ILogger<DiscordClientService> logger) : base(client, logger)
     {
-        
-
-        public BotStatusService(DiscordSocketClient client, ILogger<DiscordClientService> logger) : base(client, logger)
-        {
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            string status = LongRunningService.updateStatus;
-            await Client.WaitForReadyAsync(stoppingToken);
-            Logger.LogInformation("Client is ready!");
-
-            var fileStream = new FileStream("C:/Users/tommy/source/repos/Saer/Saer/Services/Source/KoyoriPet.png", FileMode.Open);
-            var image = new Image(fileStream);
-            await Client.SetActivityAsync(new Game(status));
-            await Client.CurrentUser.ModifyAsync(u => u.Avatar = image);
-        }
     }
 
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        string status = LongRunningService.updateStatus;
+        await Client.WaitForReadyAsync(stoppingToken);
+        Logger.LogInformation("Client is ready!");
+
+        var fileStream = new FileStream("C:/Users/tommy/source/repos/Saer/Saer/Services/Source/KoyoriPet.png", FileMode.Open);
+        var image = new Image(fileStream);
+        await Client.SetActivityAsync(new Game(status));
+        await Client.CurrentUser.ModifyAsync(u => u.Avatar = image);
+    }
 }
